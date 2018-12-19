85b72af8de3f88b0a8b2a64325bb441ba6ca3a7c
Guillermo Mosse <billy.mosse@gmail.com>
2018-12-19 20:59:25 -0300

    little script to automatize boring search and replace

---
+++ Programa_circuitos/fix_ceros.sh
@@ -0,0 +1,8 @@
+sed -i "s/\.000000000000000//g" tmp.txt
+sed -i "s/\.00000000000000//g" tmp.txt
+sed -i "s/\ \ \ /\ /g" tmp.txt
+sed -i "s/\ \ /\ /g" tmp.txt
+sed -i "s/\ /,\ /g" tmp.txt
+sed -i "s/\[,\ /\[/g" tmp.txt
+sed -i "s/\]\],/g" tmp.txt
+#Falta sacar la ultima coma del final :-(
