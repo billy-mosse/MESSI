89ca55b9d8cb28762d92b769ae04d3179c2e22d2
Guillermo Mosse <billy.mosse@gmail.com>
2019-02-21 15:06:45 -0300

    Incidence matrix working

--- code/MessiGraphUtils.py
+++ code/MessiGraphUtils.py
@@ -1,3 +1,6 @@
+
+import numpy as np
+
 class Complex:
 	def __init__(self, species):
 		"""species is a list. For example, ['S0', 'S1']
@@ -7,7 +10,32 @@ class Complex:
 
 
 def buildIncidenceMatrix(network):
-    return None
+    """
+    the matrix whose i-th column has a 1 in
+the row corresponding to the product complex of the i-th reaction and a −1 for the educt (reactant, i.e., origin)
+complex.
+
+    The network should be filled with renamed variables "0", "1", ...
+    """
+
+    #enumerate lets you loop through a structure (in this case, the edges)
+    #while simultaneously keeping the index 0, 1, 2, ...
+    #for index, edge in enumerate(nx.edges()):
+
+    rows = []
+    n_columns = len(network.nodes())
+
+    #an empty row with amount of columns equal to amount of nodes of the network
+    empty_row = [0]*n_columns
+
+    for educt, product in network.edges():
+        row = empty_row.copy()
+        row[int(educt)] = -1
+        row[int(product)] =1
+        rows.append(row)
+
+
+    return np.array(rows).transpose()
 
 def buildComplexesMatrix(network):
     return None
--- code/tests.py
+++ code/tests.py
@@ -4,35 +4,100 @@ import numpy as np
 from CircuitsInformation import CircuitsInformation
 import Utils
 
+import MessiGraphUtils
+import networkx as nx
+import numpy
+
 #TODO: write an integration test (when - some day - we write the main output in a file)
 class Test1(unittest.TestCase):
 
 
-	def test__buildup_of_conformal_circuit_to_matrix_using_Lemma_A5(self):
-		M1 = np.array([
-    		[1, 0],
-    		[0, 3]])
+    def test_buildup_of_conformal_circuit_to_matrix_using_Lemma_A5(self):
+        M1 = np.array([
+            [1, 0],
+            [0, 3]])
 
 
-		circuits_information_M1 = CircuitsInformation(M1)
+        circuits_information_M1 = CircuitsInformation(M1)
 
-		#Supposedly, this is the output of lemma A.5
-		self.assertTrue([0,-3] in circuits_information_M1.circuits)
-		self.assertTrue([0,3] in circuits_information_M1.circuits)
-		self.assertTrue([3,0] in circuits_information_M1.circuits)
-		self.assertTrue([-3,0] in circuits_information_M1.circuits)
-
-
-		M2 = np.array([
-    		[-1, 0, 2],
-    		[2, 4, 1]])
+        #Supposedly, this is the output of lemma A.5
+        self.assertTrue([0,-3] in circuits_information_M1.circuits)
+        self.assertTrue([0,3] in circuits_information_M1.circuits)
+        self.assertTrue([3,0] in circuits_information_M1.circuits)
+        self.assertTrue([-3,0] in circuits_information_M1.circuits)
 
 
-		circuits_information_M2 = CircuitsInformation(M2)
+        M2 = np.array([
+            [-1, 0, 2],
+            [2, 4, 1]])
 
-		for c in [[0, 4, 5], [0, -4, -5], [-4, 0, 8], [4, 0, -8], [-5, -8, 0], [5, 8, 0]]:
-			self.assertTrue(c in circuits_information_M2.circuits)
 
+        circuits_information_M2 = CircuitsInformation(M2)
+
+        for c in [[0, 4, 5], [0, -4, -5], [-4, 0, 8], [4, 0, -8], [-5, -8, 0], [5, 8, 0]]:
+            self.assertTrue(c in circuits_information_M2.circuits)
+
+
+
+
+
+
+    def test_buildup_of_incidence_matrix_from_network(self):
+        
+        print("(2.4) of Toric paper")
+
+        """reactions = [
+        ['S0+E', 'ES0','k1'],
+        ['ES0', 'S0+E', 'k2'],
+        ['ES0', 'S1+E','k3'],
+        ['S1+F', 'FS1', 'k4'],
+        ['FS1', 'S1+F', 'k5'],
+        ['FS1', 'S0+F', 'k6']
+        ]"""
+
+        reactions = [
+        ['0', '1','k1'],
+        ['1', '0', 'k2'],
+        ['1', '2','k3'],
+        ['3', '4', 'k4'],
+        ['4', '3', 'k5'],
+        ['4', '5', 'k6']
+        ]        
+
+        G = nx.DiGraph(directed=True)
+
+        sources = set([reaction[0] for reaction in reactions])
+
+        targets = set([reaction[1] for reaction in reactions])
+
+        nodes = sources.union(targets)
+        G.add_nodes_from(nodes)
+        for reaction in reactions:
+            G.add_edge(reaction[0], reaction[1], reaction_constant=reaction[2])
+
+        I = MessiGraphUtils.buildIncidenceMatrix(G)
+
+        I_solution = numpy.array([
+        [-1, 1, 0, 0, 0, 0],
+        [1, -1, -1, 0, 0, 0],
+        [0, 0, 1, 0, 0, 0],
+        [0, 0, 0, -1, 1, 0],
+        [0, 0, 0, 1, -1, -1],
+        [0, 0, 0, 0, 0, 1]
+        ])
+
+        #The incidence matrix might have columns in another order.
+        I_columns = I.transpose().tolist()
+        I_solution_columns = I_solution.transpose().tolist()
+
+        for I_col in I_columns:
+            self.assertTrue(I_col in I_solution_columns)
+
+        for I_solution_col in I_solution_columns:
+            self.assertTrue(I_solution_col in I_columns)
+
+        #v = numpy.array([[1,2,3],[4,5,6]])
+        #self.assertTrue([1,2,3] in v)
 
 if __name__ == '__main__':
     unittest.main()
